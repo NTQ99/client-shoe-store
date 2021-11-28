61d3384ecf362c3dfb88d4a3d9094e6e56d23df2
NTQ99 <quocnt.uet@gmail.com>
2021-11-29 01:04:10 +0700

    .

--- src/App.js
+++ src/App.js
@@ -13,26 +13,35 @@ import CartPage from "./components/pages/cart";
 import ChartPage from "./components/pages/chart";
 import CheckoutPage from "./components/pages/checkout";
 import ProductDetailPage from "./components/pages/product-detail";
+import authHeader from "./service/auth-header";
+import AuthService from './service/auth.service';
 
 import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+const AdminRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    (authHeader() && AuthService.getRoles().includes("ROLE_ADMIN"))? <Component {...props} />
+      : <Redirect to='/not-found' />
+  )} />
+)
 
 function App() {
   return (
     <Router>
         <Suspense>
           <Switch>
-            <Route exact path="/order" component={OrderPage} />
-            <Route exact path="/create-order" component={CreateOrderPage} />
-            <Route exact path="/customer" component={CustomerPage} />
-            <Route exact path="/product" component={ProductPage} />
-            <Route exact path="/delivery" component={DeliveryPage} />
+            <AdminRoute exact path="/order" component={OrderPage} />
+            <AdminRoute exact path="/create-order" component={CreateOrderPage} />
+            <AdminRoute exact path="/customer" component={CustomerPage} />
+            <AdminRoute exact path="/product" component={ProductPage} />
+            <AdminRoute exact path="/delivery" component={DeliveryPage} />
+            <AdminRoute exact path="/chart" component={ChartPage} />
             <Route exact path="/login" component={LoginPage} />
             <Route exact path="/home" component={HomePage} />
             <Route exact path="/category" component={CategoryPage} />
             <Route exact path="/cart" component={CartPage} />
-            <Route exact path="/chart" component={ChartPage} />
             <Route exact path="/checkout" component={CheckoutPage} />
             <Route strict path="/product-detail/:productCode" component={ProductDetailPage} />
             <Route exact path="/" render={() => <Redirect to='/home' />} />
